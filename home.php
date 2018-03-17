43efe6c5b01c0df1d56d5392538b9bd563bcffe9
elieber <elieber_gomesdesouza@hotmail.com>
2018-03-17 18:03:55 -0300

    Adicionando bõtoes

--- home.php
+++ home.php
@@ -1,5 +1,38 @@
 <?php include('header.php');?>
 
+<div class = "home-botoes">
+
+	<div class = "home-funcoes">
+		
+		<ul>
+			
+			<li>
+				
+				<button>
+					<div class ="home-menu">
+						<a class="home-item" href="#">Vendas</a>
+						<a class="home-item" href="#">Cliente</a>
+						<a class="home-item" href="#">Fornecedor</a>
+						<a class="home-item" href="#">Produtos</a>
+						<a class="home-item" href="#">Serviços</a>
+						
+
+					</div>
+
+				</button>
+
+
+
+			</li>
+
+
+		</ul>
+	
+
+
+	</div>
+
+</div>
 
 
 <h1>BEM VINDO AO </h1>
