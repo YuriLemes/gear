7f158a3caca4a11e6273c4c4006ef5830f40b6be
GabrielHS03 <GabrielHS03@users.noreply.github.com>
2018-03-27 17:30:28 -0300

    criação serviço cadastro

---
+++ servico-cadastro.php
@@ -0,0 +1,49 @@
+<?php include('header.php');?>
+<div class="containerprincipal">
+	<h5>Cadastro de Serviço</h5>
+	<form method="post" class="form-inline" role="form" action="cadastrar-servico" id="form-servico">
+		<div class="containern">
+			<fieldset>
+				<div class="form-group">
+					<label for="servico">Serviço:</label>
+					<input type="text" name="cservico" id="servico" class="form-control" required maxlength="25" />
+					<span class="error">
+						*
+					</span>
+				</div>
+				<div class="form-group">
+					<label for="descricao">Descrição:</label>
+					<input type="text" name="cdesc" id="desc" class="form-control" rows="5" required maxlength="250" />
+				</div>
+			</fieldset>
+		</div>
+	</form>
+	<p>
+			<span class="error"> 
+				
+			</span><font>
+					*Campos Obrigatórios
+				</font>
+		</p>
+		<div class="containerb centralizar">
+			<fieldset>
+				<button type="button" class="btn btn-primary" id="btn-alterar" ">
+					<span>
+						<i class="fas fa-edit"></i> Alterar
+					</span>
+				</button>
+				<button type="submit" class="btn btn-success" id="btn-salvar">
+					<span>
+						<i class="far fa-save"></i> Salvar
+					</span>
+				</button>
+				<button type="button" class="btn btn-danger" id="btn-cancelar" onclick="window.location.href='servico-lista.php';">
+					<span>
+						<i class="fas fa-times"></i> Cancelar
+					</span>
+				</button>
+			</fieldset>
+		</div>
+	</form>
+</div>
+	<?php include('footer.php')?>
