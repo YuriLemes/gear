4b16f55c59b62ea511a9c2cbceaa8f4fd208327f
IzabelaAndrade14 <33361076+IzabelaAndrade14@users.noreply.github.com>
2018-04-03 19:55:06 -0300

    Inclusão parcial ação cadastrar usuario

---
+++ usuario-acao-cadastrar.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+require('conecta_db.php');
+require('Usuario.php');
+require('UsuarioBO.php');
+
+$nome = $_POST['cnome'];
+$login = $_POST['clogin'];
+$senha = $_POST['csenha'];
+$ativo = $_POST['cativo'];
+$perfil = $_POST['cperfil'];
+$cnpj = $_POST['ccnpj'];
+
+
+
+
+$usuario = new Usuario();
+$usuario->setNome($nome);
+$usuario->setLogin($login);
+$usuario->setSenha($senha);
+$usuario->setAtivo($ativo);
+$usuario->setPerfil($perfil);
+$usuario->setCnpjEmpresa($ccnpj);
+
+try{
+    UsuarioBO::save($usuario);
+    echo "chegou no try";
+
+}catch (Exception $exception){
+    $_SESSION['excecao']['mensagem'] = $exception->getMessage();
+}
+/*header('Location: usuario-lista');*/
