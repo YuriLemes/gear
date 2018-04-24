a00285c6821521bb72d0ea0b368f841291940b1a
Elieber gomes <36684104+ElieberGomes@users.noreply.github.com>
2018-04-24 15:24:21 -0300

    edição cliente visualização

--- cliente-form-visualizacao.php
+++ cliente-form-visualizacao.php
@@ -1,18 +1,11 @@
 <?php 
 include('header-form.php');
 require ('ClienteBO.php');
+$id=$_GET['id'];
+$servico= ClienteBO::findById($id);
 ?>
  <div class="containerprincipal">
  	
 
  </div>
-
-
-
-
-
-
-
-
-
 <?php include('footer.php')?>
